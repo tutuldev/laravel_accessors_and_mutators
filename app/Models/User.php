83d3103f5724c2e5a4396a59dff6addf7c555b17
MD ABDUR RAHAMAN <abdurrahaman444.spy.com>
2025-02-03 10:10:09 +0600

    set name,email small letter when insert and password encrypt

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,4 +11,18 @@ class User extends Model
     // protected $fillable = ['name','email','age','city'];
     public  $timestamps=false;
     protected $guarded = [];
+    // convert with small letter in email column
+    public function setEmailAttribute($value){
+        $this->attributes['email']= strtolower($value);
+    }
+    // convert with small letter in name  column
+    public function setUserNameAttribute($value){
+        $this->attributes['user_name']= strtolower($value);
+    }
+
+    // password bcrypt or incrypt
+    public function setPasswordAttribute($value){
+        $this->attributes['password']= bcrypt($value);
+    }
+
 }
